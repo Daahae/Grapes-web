0c099cbe99e84fd5cc88304b9701cd5a04d0c89e
KangInhyeok <rkddlsgur983@gmail.com>
2018-07-11 19:04:53 +0900

    라이트박스추가
    라이트박스추가

---
+++ js/lightbox.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+      function showLightBox() {
+        $('#darken-background').show();
+        $('#darken-background').css('top', $(window).scrollTop());
+
+        $('body').css('overflow', 'hidden');
+      }
+
+      function hideLightBox() {
+        $('#darken-background').hide();
+
+        $('body').css('overflow', '');
+      }
+
+      $('#darken-background').click(function() {
+        hideLightBox();
+      });
+
+      $('.paper').click(function() { showLightBox(); });
+
+      $('#lightbox').click(function(event) {
+        event.stopPropagation()
+      });
+    });
